00e88e12f9d5b364c91ce38dd14e2ac938e63e36
Carine Casagrande <carine.casagrande@hotmail.com>
2021-10-11 19:35:19 -0300

    finalizando o projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,15 @@
 function calc() {
-    // var ponto = Number.isNaN(parseFloat($("#ponto").val())) ? 0 : parseFloat($("#ponto").val());
-   var ponto = 0;
+    var x = 0;
+    var x0 = Number.isNaN(parseFloat($("#x0").val())) ? 0 : parseFloat($("#x0").val());
     var x1 = Number.isNaN(parseFloat($("#x1").val())) ? 0 : parseFloat($("#x1").val());
     var x2 = Number.isNaN(parseFloat($("#x2").val())) ? 0 : parseFloat($("#x2").val());
-    var x3 = Number.isNaN(parseFloat($("#x3").val())) ? 0 : parseFloat($("#x3").val());
     var y1 = Number.isNaN(parseFloat($("#y1").val())) ? 0 : parseFloat($("#y1").val());
     var y2 = Number.isNaN(parseFloat($("#y2").val())) ? 0 : parseFloat($("#y2").val());
     var y3 = Number.isNaN(parseFloat($("#y3").val())) ? 0 : parseFloat($("#y3").val());
 
-    let a = ((ponto - x2) * (ponto - x3)) / ((x1 - x2) * (x1 - x3));
-    let b = ((ponto - x1) * (ponto - x3)) / ((x2 - x1) * (x2 - x3));
-    let c = ((ponto - x1) * (ponto - x2)) / ((x3 - x1) * (x3 - x2));
+    let a = ((x - x1) * (x - x2)) / ((x0 - x1) * (x0 - x2));
+    let b = ((x - x0) * (x - x2)) / ((x1 - x0) * (x1 - x2));
+    let c = ((x - x0) * (x - x1)) / ((x2 - x0) * (x2 - x1));
 
     let d = (a * y1) + (b * y2) + (c * y3);
 
