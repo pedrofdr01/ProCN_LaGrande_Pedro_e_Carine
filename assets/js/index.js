d0f8352be2bcb4f7b2f734615dab6261cc1872f3
Carine Casagrande <carine.casagrande@hotmail.com>
2021-10-11 19:47:49 -0300

    ajustes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,15 +3,15 @@ function calc() {
     var x0 = Number.isNaN(parseFloat($("#x0").val())) ? 0 : parseFloat($("#x0").val());
     var x1 = Number.isNaN(parseFloat($("#x1").val())) ? 0 : parseFloat($("#x1").val());
     var x2 = Number.isNaN(parseFloat($("#x2").val())) ? 0 : parseFloat($("#x2").val());
+    var y0 = Number.isNaN(parseFloat($("#y0").val())) ? 0 : parseFloat($("#y0").val());
     var y1 = Number.isNaN(parseFloat($("#y1").val())) ? 0 : parseFloat($("#y1").val());
     var y2 = Number.isNaN(parseFloat($("#y2").val())) ? 0 : parseFloat($("#y2").val());
-    var y3 = Number.isNaN(parseFloat($("#y3").val())) ? 0 : parseFloat($("#y3").val());
 
     let a = ((x - x1) * (x - x2)) / ((x0 - x1) * (x0 - x2));
     let b = ((x - x0) * (x - x2)) / ((x1 - x0) * (x1 - x2));
     let c = ((x - x0) * (x - x1)) / ((x2 - x0) * (x2 - x1));
 
-    let d = (a * y1) + (b * y2) + (c * y3);
+    let d = (a * y0) + (b * y1) + (c * y2);
 
     printOutput(d);
 }
